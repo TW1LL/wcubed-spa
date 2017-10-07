5fe9b10bd8597dca72f90350c866d0d887afdc83
Will Wagner <willwagner602@gmail.com>
2017-10-07 16:43:13 -0400

    Set host in environments

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,13 +1,23 @@
-export const host = 'http://localhost:3000';
+import {testEnvironment} from '../../environments/environment'
+import {prodEnvironment} from '../../environments/environment.prod.ts'
+import { isDevMode } from '@angular/core';
+
+if ( isDevMode() ) {
+  environment = testEnvironment
+}
+else {
+  environment = prodEnvironment
+}
+
 export const API = {
-  product: host + '/product',
-  order: host + '/order',
-  orderCreate: host + '/order/create',
-  category: host + '/category',
-  user: host + '/user',
-  userLogin: host + '/user/login',
-  userRegister: host + '/user/register',
-  shipmentCreate: host + '/shipment/create',
-  shipmentBuy: host + '/shipment/buy',
-  purchase: host + '/payment/purchase'
+  product: environment.host + '/product',
+  order: environment.host + '/order',
+  orderCreate: environment.host + '/order/create',
+  category: environment.host + '/category',
+  user: environment.host + '/user',
+  userLogin: environment.host + '/user/login',
+  userRegister: environment.host + '/user/register',
+  shipmentCreate: environment.host + '/shipment/create',
+  shipmentBuy: environment.host + '/shipment/buy',
+  purchase: environment.host + '/payment/purchase'
 };
