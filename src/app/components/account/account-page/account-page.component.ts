8efbd84cbd550a682a713324eb2349092352cedc
Will Wagner <willwagner602@gmail.com>
2017-10-07 22:28:14 -0400

    Added the component I missed earlier

---
+++ src/app/components/account/account-page/account-page.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from "../../../models";
+import {OrderService} from "../../../services/order.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-account-page',
+  template: `
+    <div class="container">
+        <div class="row">
+            <div class="col m8 s12 right">
+  
+                <mz-card 
+                    [backgroundClass]="'blue-grey darken-1'">
+                    <mz-card-content>
+                        <span class="card-title">{{ user.email}}'s information</span>
+                        <div class="card-panel">
+                        </div>
+                    </mz-card-content>
+                </mz-card>
+                
+                <div class="card blue-grey darken-1">
+                    <div class="card-content">
+                        <mz-card-title>Recent Orders</mz-card-title>
+                        <div class="row">
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="card blue-grey darken-1">
+                    <div class="card-content">
+                        <span class="card-title">Recent Orders</span>
+                        <div class="row">
+                            <div class="col m6 s12" *ngFor="let order of user.orders">
+                                <div class="card-panel">
+                                    <a href="/order/{{order.id}}">
+                                    <img src="{{ order.image }}" class="responsive-img" />
+                                    <p>Total: $ {{order.total}}</p>
+                                    <p>Ordered {{order.created}}</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class=" col s12 m4 left">
+                <div class="collection">
+                    <a href="/cart" class="collection-item">View My Cart</a>
+                    <a href="/account/password/change" class="collection-item">Change Password</a>
+                    <a href="/account/change_info" class="collection-item">Update Information</a>
+                    <a href="/order/history" class="collection-item">Order History</a>
+                    <a href="/account/logout" class="collection-item">Logout</a>
+                </div>
+            </div>
+          
+        </div>
+    </div>
+  `,
+  styles: []
+})
+
+export class AccountPageComponent implements OnInit {
+
+  user: User;
+
+
+  constructor(private route: ActivatedRoute, private orderService: OrderService) { }
+
+  ngOnInit() {
+    this.user = this.route.snapshot.data['user'];
+  }
+
+}
