ecb6ef58a60e28e66f4021c657ebfedd891d2b4b
Tom Fitzpatrick <tfitz237@gmail.com>
2017-10-15 22:38:25 -0400

    ngmodules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <navbar></navbar>
+    <wcubed-app></wcubed-app>
+    <wcubed-footer></wcubed-footer>
+  `
 })
 export class AppComponent {
-  title = 'app';
-
-  gotoCategories() {
-
-  }
 }
+
